16705aaf2057fdacad17e0fff10491a0400d29c0
Pauljim98 <paulsoy_10@outlook.es>
2023-12-12 18:46:23 -0500

    commit 18:45

--- app/src/main/java/fisei/maksym/cannongame/MainActivity.java
+++ app/src/main/java/fisei/maksym/cannongame/MainActivity.java
@@ -1,6 +1,6 @@
 package fisei.maksym.cannongame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.maksym.cannongame.R;
